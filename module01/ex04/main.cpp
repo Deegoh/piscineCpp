1bd168b2535f0892f9ee9cd4166de69cae20bf00
Tiago Pinto Monteiro <tpinto-m@student.42lausanne.ch>
2022-09-22 14:50:12 +0200

    01ex04

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
 #include <fstream>
 
-void stream_editor(std::string filename, std::string s1, std::string s2)
+bool stream_editor(std::string filename, std::string s1, std::string s2)
 {
 	std::ifstream ifs(filename.c_str());
 	std::string readed;
+
+	if (ifs.fail() || s1.empty())
+		return 0;
+	char c = ifs.get();
 	do {
-	ifs >> readed;
-	std::cout << readed;
-	} while (!readed.empty());
+		readed += c;
+		c = ifs.get();
+	} while (!ifs.eof());
 	ifs.close();
 
+	for (size_t pos = readed.find(s1); pos != readed.npos; )
+	{
+		readed.erase (pos, s1.length());
+		readed.insert(pos, s2);
+		pos = readed.find(s1);
+	}
+
 	std::ofstream ofs(filename.append(".replace").c_str());
-	ofs << "i like ponies a whole damn lot" << std::endl;
+	if (ofs.fail())
+		return 0;
+	ofs << readed;
 	ofs.close();
-	(void)s1;
-	(void)s2;
+	return 1;
 }
 
-int main()
+int main(int ac, char **av)
 {
-	stream_editor("test", "null", "null");
-	//s1 = s2
+	if (ac != 4)
+	{
+		std::cout << "./usage <filename> <s1> <s2>" << std::endl;
+		return 1;
+	}
+	std::string filename(av[1]);
+	std::string s1(av[2]);
+	std::string s2(av[3]);
+	if (!stream_editor(filename, s1, s2))
+	{
+		std::cout << "Failed" << std::endl;
+		return 1;
+	}
+	std::cout << "Succeed" << std::endl;
+	return 0;
 }
