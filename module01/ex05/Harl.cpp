5d5fb7e66fe2796cf6b0135bd05d689bab95e14c
Tiago Pinto Monteiro <tpinto-m@student.42lausanne.ch>
2022-09-22 16:00:02 +0200

    wip 01ex05 harl

---
+++ module01/ex05/Harl.cpp
@@ -0,0 +1,11 @@
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void Harl::complain(std::string level) {
+	(void)level;
+}
+
+
---
+++ module01/ex05/main.cpp
@@ -0,0 +1,6 @@
+#include "Harl.hpp"
+
+int main() {
+	std::cout << "Hello world" << std::endl;
+	return 0;
+}
