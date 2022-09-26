47d4018fc9fd62087e7e0702bfbbd092ceabb437
Tiago Pinto Monteiro <tpinto-m@student.42lausanne.ch>
2022-09-26 13:52:09 +0200

    improve 01ex05 harl

--- module01/ex05/Harl.cpp
+++ module01/ex05/Harl.cpp
@@ -22,18 +22,20 @@ void Harl::complain(std::string level) {
 }
 
 void Harl::debug() {
-	std::cout << "hello debug" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << std::endl;
 }
 
 void Harl::info() {
-	std::cout << "hello info" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put "
+				 "enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
 }
 
 void Harl::warning() {
-	std::cout << "hello warning" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming "
+				 "for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error() {
-	std::cout << "hello error" << std::endl;
+	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
 }
 
--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -5,9 +5,16 @@
 int main() {
 	Harl harl;
 
+	std::cout << "INFO TEST" << std::endl;
 	harl.complain("INFO");
+	std::cout << "ERROR TEST" << std::endl;
 	harl.complain("ERROR");
+	std::cout << "WARNING TEST" << std::endl;
 	harl.complain("WARNING");
+	std::cout << "DEBUG TEST" << std::endl;
 	harl.complain("DEBUG");
+	std::cout << "MONKEY TEST" << std::endl;
+	harl.complain("asd");
+	std::cout << "TESTS FINISHED" << std::endl;
 	return 0;
 }
