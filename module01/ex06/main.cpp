f9581f70bf5bcd4c779b215cf9baefe65039397c
Tiago Pinto Monteiro <tpinto-m@student.42lausanne.ch>
2022-09-29 16:36:14 +0200

    fix 01ex06 switch

--- module01/ex06/main.cpp
+++ module01/ex06/main.cpp
@@ -1,8 +1,23 @@
 #include "Harl.hpp"
 
-int main() {
-	Harl harl;
+void tester(Harl harl);
 
+int main (int ac, char **av)
+{
+	Harl harl;
+	std::string user_input;
+
+	if (ac == 2)
+	{
+		user_input = av[1];
+		harl.complain(user_input);
+	}
+	else
+		harl.complain("");
+//	tester(harl);
+}
+
+void tester(Harl harl) {
 	std::cout << "DEBUG TEST" << std::endl;
 	harl.complain("DEBUG");
 	std::cout << std::endl;
@@ -24,5 +39,4 @@ int main() {
 	std::cout << std::endl;
 
 	std::cout << "TESTS FINISHED" << std::endl;
-	return 0;
 }
