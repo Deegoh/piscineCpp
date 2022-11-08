193a66acd785c5b0c337cfe3737108cfca7d0eb6
Tiago Pinto Monteiro <tpinto-m@student.42lausanne.ch>
2022-11-08 17:32:48 +0100

    08ex00 add easyfind

--- module08/ex00/inc/easyfind.hpp
+++ module08/ex00/inc/easyfind.hpp
@@ -1,9 +1,30 @@
-#ifdef EASYFIND_HPP
-#define EASYFIND_HPP
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <stdexcept>
+# include <iostream>
 
 template<typename T>
-void easyfinf(T type, "param") {
+void display_container(T &container) {
+	typename T::iterator it;
+	for (it = container.begin(); it != container.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
+}
 
+template<typename T>
+void easyfind(T &container, int n) {
+	typename T::iterator ite = container.end();
+	for (typename T::iterator it = container.begin(); it != ite; ++it)
+	{
+		if (*it == n)
+		{
+			std::cout << *it << std::endl;
+			return;
+		}
+	}
+	throw std::exception();
 }
 
 #endif
--- module08/ex00/src/main.cpp
+++ module08/ex00/src/main.cpp
@@ -1,5 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include "easyfind.hpp"
+
+void vector() {
+	std::vector<int> v1;
+
+	v1.push_back(1);
+	v1.push_back(17);
+	v1.push_back(42);
+	v1.push_back(100);
+	std::cout << "Vector test" << std::endl;
+	try {
+		easyfind(v1, 20);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << ": found nothing." << std::endl;
+	}
+	try {
+		easyfind(v1, 42);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << ": found nothing." << std::endl;
+	}
+}
+
+void list() {
+	std::list<int> lst1;
+
+	lst1.push_back(1);
+	lst1.push_back(17);
+	lst1.push_back(42);
+	lst1.push_back(100);
+	std::cout << "list test" << std::endl;
+	try {
+		easyfind(lst1, 20);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << ": found nothing." << std::endl;
+	}
+	try {
+		easyfind(lst1, 42);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << ": found nothing." << std::endl;
+	}
+}
 
 int main() {
-	std::cout << "Hello template" << std::endl;
+	vector();
+	list();
 }
